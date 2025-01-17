601055d1ffe396003a6cbb06406514f5fb9b4522
Mink <mink@demaneschijn.nl>
2025-01-17 14:10:49 +0100

    created layered network

--- concept/layered_network.py
+++ concept/layered_network.py
@@ -5,6 +5,7 @@ Plot multi-graphs in 3D.
 import numpy as np
 import matplotlib.pyplot as plt
 import networkx as nx
+import random
 
 from mpl_toolkits.mplot3d import Axes3D
 from mpl_toolkits.mplot3d.art3d import Line3DCollection
@@ -12,38 +13,17 @@ from mpl_toolkits.mplot3d.art3d import Line3DCollection
 
 class LayeredNetworkGraph(object):
 
-    def __init__(self, graphs, node_labels=None, layout=nx.spring_layout, ax=None):
-        """Given an ordered list of graphs [g1, g2, ..., gn] that represent
-        different layers in a multi-layer network, plot the network in
-        3D with the different layers separated along the z-axis.
-
-        Within a layer, the corresponding graph defines the connectivity.
-        Between layers, nodes in subsequent layers are connected if
-        they have the same node ID.
-
-        Arguments:
-        ----------
-        graphs : list of networkx.Graph objects
-            List of graphs, one for each layer.
-
-        node_labels : dict node ID : str label or None (default None)
-            Dictionary mapping nodes to labels.
-            If None is provided, nodes are not labelled.
-
-        layout_func : function handle (default networkx.spring_layout)
-            Function used to compute the layout.
-
-        ax : mpl_toolkits.mplot3d.Axes3d instance or None (default None)
-            The axis to plot to. If None is given, a new figure and a new axis are created.
-
-        """
-
-        # book-keeping
+    def __init__(self, graphs):
         self.graphs = graphs
         self.total_layers = len(graphs)
 
-        self.node_labels = node_labels
-        self.layout = layout
+        self.get_nodes()
+        self.get_edges_within_layers()
+        self.get_edges_between_layers()
+
+        """REMOVE THIS ALL LATER:"""
+        self.node_labels = {nn : str(nn) for nn in range(4*n)}
+        self.layout = nx.spring_layout
 
         if ax:
             self.ax = ax
@@ -51,52 +31,38 @@ class LayeredNetworkGraph(object):
             fig = plt.figure()
             self.ax = fig.add_subplot(111, projection='3d')
 
-        # create internal representation of nodes and edges
-        self.get_nodes()
-        self.get_edges_within_layers()
-        self.get_edges_between_layers()
-
         # compute layout and plot
         self.get_node_positions()
         self.draw()
 
-
     def get_nodes(self):
-        """Construct an internal representation of nodes with the format (node ID, layer)."""
         self.nodes = []
-        for z, g in enumerate(self.graphs):
-            self.nodes.extend([(node, z) for node in g.nodes()])
-
-
+        for i, g in enumerate(self.graphs):
+            self.nodes.extend([(node, i) for node in g.nodes()])
+    
     def get_edges_within_layers(self):
-        """Remap edges in the individual layers to the internal representations of the node IDs."""
         self.edges_within_layers = []
-        for z, g in enumerate(self.graphs):
-            self.edges_within_layers.extend([((source, z), (target, z)) for source, target in g.edges()])
-
-
-    def get_edges_between_layers(self):
-        """Determine edges between layers. Nodes in subsequent layers are
-        thought to be connected if they have the same ID."""
+        for i, g in enumerate(self.graphs):
+            self.edges_within_layers.extend([((post_syn, i), (pre_syn, i)) for post_syn, pre_syn in g.edges()])
+    
+    def get_edges_between_layers(self, prob=0.1):
+        #TODO: implement this function with the desired amount of connections and with multiple connections per node
         self.edges_between_layers = []
         for z1, g in enumerate(self.graphs[:-1]):
             z2 = z1 + 1
             h = self.graphs[z2]
-            shared_nodes = set(g.nodes()) & set(h.nodes())
-            self.edges_between_layers.extend([((node, z1), (node, z2)) for node in shared_nodes])
 
+            for node1 in g.nodes():
+            # Iterate over all nodes in layer z2 (next layer)
+                for node2 in h.nodes():
+                # Randomly connect node1 and node2 with probability prob
+                    if random.random() < prob:
+                        self.edges_between_layers.append(((node1, z1), (node2, z2)))
+
+            # shared_nodes = set(g.nodes()) & set(h.nodes())
+            # self.edges_between_layers.extend([((node, z1), (node, z2)) for node in shared_nodes])
 
     def get_node_positions(self, *args, **kwargs):
-        """Get the node positions in the layered layout."""
-        # What we would like to do, is apply the layout function to a combined, layered network.
-        # However, networkx layout functions are not implemented for the multi-dimensional case.
-        # Futhermore, even if there was such a layout function, there probably would be no straightforward way to
-        # specify the planarity requirement for nodes within a layer.
-        # Therefor, we compute the layout for the full network in 2D, and then apply the
-        # positions to the nodes in all planes.
-        # For a force-directed layout, this will approximately do the right thing.
-        # TODO: implement FR in 3D with layer constraints.
-
         composition = self.graphs[0]
         for h in self.graphs[1:]:
             composition = nx.compose(composition, h)
@@ -107,17 +73,14 @@ class LayeredNetworkGraph(object):
         for z, g in enumerate(self.graphs):
             self.node_positions.update({(node, z) : (*pos[node], z) for node in g.nodes()})
 
-
     def draw_nodes(self, nodes, *args, **kwargs):
-        x, y, z = zip(*[self.node_positions[node] for node in nodes])
-        self.ax.scatter(x, y, z, *args, **kwargs)
-
+            x, y, z = zip(*[self.node_positions[node] for node in nodes])
+            self.ax.scatter(x, y, z, *args, **kwargs)
 
     def draw_edges(self, edges, *args, **kwargs):
         segments = [(self.node_positions[source], self.node_positions[target]) for source, target in edges]
         line_collection = Line3DCollection(segments, *args, **kwargs)
         self.ax.add_collection3d(line_collection)
-
 
     def get_extent(self, pad=0.1):
         xyz = np.array(list(self.node_positions.values()))
@@ -128,7 +91,6 @@ class LayeredNetworkGraph(object):
         return (xmin - pad * dx, xmax + pad * dx), \
             (ymin - pad * dy, ymax + pad * dy)
 
-
     def draw_plane(self, z, *args, **kwargs):
         (xmin, xmax), (ymin, ymax) = self.get_extent(pad=0.1)
         u = np.linspace(xmin, xmax, 10)
@@ -137,42 +99,47 @@ class LayeredNetworkGraph(object):
         W = z * np.ones_like(U)
         self.ax.plot_surface(U, V, W, *args, **kwargs)
 
-
     def draw_node_labels(self, node_labels, *args, **kwargs):
         for node, z in self.nodes:
             if node in node_labels:
                 ax.text(*self.node_positions[(node, z)], node_labels[node], *args, **kwargs)
 
-
     def draw(self):
-
-        self.draw_edges(self.edges_within_layers,  color='k', alpha=0.3, linestyle='-', zorder=2)
-        self.draw_edges(self.edges_between_layers, color='k', alpha=0.3, linestyle='--', zorder=2)
+        self.draw_edges(self.edges_within_layers,  color='k', alpha=0.05, linestyle='-', zorder=2)
+        self.draw_edges(self.edges_between_layers, color='k', alpha=0.05, linestyle='--', zorder=2)
 
         for z in range(self.total_layers):
             self.draw_plane(z, alpha=0.2, zorder=1)
-            self.draw_nodes([node for node in self.nodes if node[1]==z], s=300, zorder=3)
+            self.draw_nodes([node for node in self.nodes if node[1]==z], s=30, zorder=3)
 
-        if self.node_labels:
-            self.draw_node_labels(self.node_labels,
-                                  horizontalalignment='center',
-                                  verticalalignment='center',
-                                  zorder=100)
-
+            # if self.node_labels:
+            #     self.draw_node_labels(self.node_labels,
+            #                         horizontalalignment='center',
+            #                         verticalalignment='center',
+            #                         zorder=100)
 
 if __name__ == '__main__':
-
     # define graphs
-    n = 5
-    g = nx.erdos_renyi_graph(4*n, p=0.1)
-    h = nx.erdos_renyi_graph(3*n, p=0.2)
-    i = nx.erdos_renyi_graph(2*n, p=0.4)
+    n = 30
+    g = nx.erdos_renyi_graph(n, p=0.3)
+    h = nx.erdos_renyi_graph(n, p=0.3)
+    i = nx.erdos_renyi_graph(n, p=0.3)
 
     node_labels = {nn : str(nn) for nn in range(4*n)}
 
     # initialise figure and plot
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    LayeredNetworkGraph([g, h, i], node_labels=node_labels, ax=ax, layout=nx.spring_layout)
+    LayeredNetworkGraph([g, h, i])
     ax.set_axis_off()
     plt.show()
+
+
+
+
+
+
+
+
+
+
