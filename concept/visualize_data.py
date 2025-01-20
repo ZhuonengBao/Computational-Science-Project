30ab651c376519530c82eb6538c56783fd1a8d57
babetw <babet.wijsman@hetnet.nl>
2025-01-20 17:07:50 +0100

    added plot with time interval for different network variable (not fully checked yet)

--- concept/visualize_data.py
+++ concept/visualize_data.py
@@ -2,9 +2,13 @@ import matplotlib.pyplot as plt
 from hh_model import HodgkinHuxleyNeuron
 from layered_network import LayeredNetworkGraph
 import networkx as nx
+import pandas as pd
+import numpy as np
 
 def visualize_hh_network(network, n):
-
+    """
+    This funtion plots the voltage over time for the first and last node of each layer in a network.
+    """
     # Run the Hodgkin-Huxley model and get the data
     V_record, time = network.run_hh_network()
     selected_indices = []
@@ -27,7 +31,9 @@ def visualize_hh_network(network, n):
     plt.show()
 
 def calc_potentials_per_layer(network, n):
-
+    """
+    This function calculated the time it takes for each layer of the network to be fully activated
+    """
     V_record, time = network.run_hh_network()
     layer_time = []
 
@@ -55,7 +61,10 @@ def calc_potentials_per_layer(network, n):
     plt.title("Spike Timing Across Layers")
     plt.show()
 
-def time_between_spiking(network, n, start, end):
+def time_between_spiking(network, start, end):
+    """
+    This function calculated the time difference in spiking of a start and end neuron
+    """
     V_record, time = network.run_hh_network()
 
     for t, V in zip(time, V_record[start]):
@@ -68,8 +77,39 @@ def time_between_spiking(network, n, start, end):
             end_time = t
             break
 
-    print(end_time - start_time, 'ms')
+        end_time = start_time
 
+    time_interval = end_time - start_time
+    print(end_time - start_time, 'ms')
+    return time_interval
+
+def plot_spiking_time(n, start, end, trials):
+    properties = np.linspace(0, 1, 5)
+    data = []
+
+    for connectivity in properties:
+        results = []
+        for _ in range(trials):
+            g = nx.erdos_renyi_graph(n, p= connectivity)
+            h = nx.erdos_renyi_graph(n, p= connectivity)
+            i = nx.erdos_renyi_graph(n, p= connectivity)
+
+            # Create the layered network
+            network = LayeredNetworkGraph([g, h, i])
+
+            time_interval = time_between_spiking(network, n, start, end)
+            data.append(time_interval)
+
+            results.append((connectivity, np.mean(data), np.std(data)))
+
+        df = pd.DataFrame(results, columns=["Connectivity", "Mean", "Std"])
+
+        # Plot the results
+        plt.errorbar(df["Connectivity"], df["Mean"], yerr=df["Std"], fmt='o', capsize=5, label="Spike Timing")
+        plt.xlabel('connectivity')
+        plt.ylabel('time between first and last neuron')
+        plt.show()
+   
 
 if __name__ == "__main__":
     n = 50
@@ -79,6 +119,8 @@ if __name__ == "__main__":
 
     # Create the layered network
     network = LayeredNetworkGraph([g, h, i])
+    
     #visualize_hh_network(network, n)
     #calc_potentials_per_layer(network, n)
-    time_between_spiking(network, n, (0,0), (40,2))
+    #time_between_spiking(network, n, (0,0), (40,2))
+    plot_spiking_time(n, (0,0), (49,2), 3)
